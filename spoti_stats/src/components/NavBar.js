8ad3e684f79c4db2d3230795dd9b0f6f395e8977
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-09-16 00:29:10 +0200

    changed name to SpotiStats

--- spoti_stats/src/components/NavBar.js
+++ spoti_stats/src/components/NavBar.js
@@ -4,7 +4,7 @@ function NavBar({clientId}) {
     return (
       <div className='bg-my-red flex flex-auto flex-row justify-evenly'>   
       <div className=' p-5 md:mx-40 min-w-600 rounded-md justify-between'>  
-          <p className=" text-3xl font-bold"> Spotifier 3000</p>
+          <p className=" text-3xl font-bold"> SpotiStats</p>
           <button className='bg-my-dark rounded-full p-2 text-my-green m-2 font-bold'
           onClick={()=> redirectToAuthCodeFlow(clientId)}>Login to Spotify</button>           
           </div>
