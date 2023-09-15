6b21ac51930434c6d169fe7bcab3b053560c5ccc
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-09-15 23:56:30 +0200

    improved buttons in navbar

--- spotifier3000/src/components/NavBar.js
+++ spotifier3000/src/components/NavBar.js
@@ -1,8 +1,10 @@
 function NavBar() {
     return (
-      <div className='bg-my-red flex flex-auto justify-evenly'>   
-          <h1> Spotifier 3000</h1>
-          <h3 className='text-my-green font-bold'>Login</h3>           
+      <div className='bg-my-red flex flex-auto flex-row justify-evenly'>   
+      <div className=' p-5 md:mx-40 min-w-600 rounded-md justify-between'>  
+          <p className=" text-3xl font-bold"> Spotifier 3000</p>
+          <button className='bg-my-dark rounded-full p-2 text-my-green m-2 font-bold'>Login to Spotify</button>           
+          </div>
       </div>
     );
   }
--- spotifier3000/tailwind.config.js
+++ spotifier3000/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       "my-red": " #FF0060",
       "my-yellow": "#F6FA70",
       "my-green": "#00DFA2",
+      "my-dark":"#21262D",
 
     },
     extend: {},
