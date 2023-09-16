77e404ad74c33a776ecbee09e3dcf5791ba89d53
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-09-16 20:12:50 +0200

    genres code cleanup

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Project made with Tailwind.
 Features checklist:
 
 - [X]  show top 50, 25, 10, 5 songs and artists in the context of the last 1, 6 or 12 months
-- [ ]  downloading top 50 songs from last 12 months and count most popular generes
+- [X]  downloading top 50 songs from last 12 months and count most popular generes
 - [ ]  heatmap with most listened artists
 - [ ]  creating playlist with your most listened ever songs
 - [ ]  recomendactions (playlist)
--- spoti_stats/src/helper/aggregateGeneres.js
+++ spoti_stats/src/helper/aggregateGeneres.js
@@ -1,23 +1,24 @@
-function aggregateGeneres(listOfArtists){
-    const allGenres = [];
-    const genreCounts = {};
-    listOfArtists.forEach(obj => {
-      allGenres.push(...obj.genres);
-    });
+function aggregateGeneres(listOfArtists) {
+  const allGenres = [];
+  const genreCounts = {};
+  listOfArtists.forEach((obj) => {
+    allGenres.push(...obj.genres);
+  });
 
-    allGenres.forEach(genre => {
-      if (genreCounts[genre]) {
-        genreCounts[genre]++;
-      } else {
-        genreCounts[genre] = 1;
-      }
-    });
-    
-    const sortedGenreCounts = Object.entries(genreCounts)
-      .sort((a, b) => b[1] - a[1]);
-    const topTen = sortedGenreCounts.slice(0, 10);
+  allGenres.forEach((genre) => {
+    if (genreCounts[genre]) {
+      genreCounts[genre]++;
+    } else {
+      genreCounts[genre] = 1;
+    }
+  });
 
-    return topTen
-  }
+  const sortedGenreCounts = Object.entries(genreCounts).sort(
+    (a, b) => b[1] - a[1]
+  );
+  const topTen = sortedGenreCounts.slice(0, 10);
 
-  export default aggregateGeneres;
+  return topTen;
+}
+
+export default aggregateGeneres;
