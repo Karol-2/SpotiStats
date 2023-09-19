a5d2b0e063ef5552791814cef1d511446b6f1e50
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-09-19 22:36:39 +0200

    fixed artists names and added link to song on spotify

--- spoti_stats/src/components/TopSongs.js
+++ spoti_stats/src/components/TopSongs.js
@@ -32,9 +32,12 @@ function TopArtists() {
   }
   return (
     token && (
+      
       <div className="background-animation-green p-5 md:mx-40 min-w-600 shadow-2xl m-10 rounded-xl  ">
         <div className="flex ">
           <div className=" w-1/2 p-4">
+          {console.log(songs)}
+
             <div className="h-full">
               {songs ? (
                 songs.items.map((val, key) => {
